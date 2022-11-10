c922e9030ae28b33af555251e2acfc7922604a0d
EI13131 <EI13131@ELW5112.corpindia.local>
2022-11-10 09:30:01 +0530

    Commited Assignment

--- banking/src/demo/BankAccountOperations.java
+++ banking/src/demo/BankAccountOperations.java
@@ -2,6 +2,7 @@ package demo;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BankAccountOperations {
@@ -23,5 +24,150 @@ public class BankAccountOperations {
 		}
 		return rows;
 	}
+	
+	public static float getBalance(int accountNumber) {
+		float balance = -1;
+		
+		try {
+			Connection con = DBConnection.getCon();
+			String sql = "select balance from bank_account where acc_no = ?";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, accountNumber);
+			ResultSet rs =  ps.executeQuery();
+			while(rs.next()) {
+				balance = rs.getFloat(1);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return balance;
+	}
+	
+	
+	public static void getAccountInfoByAccountNumber(int accountNumber) {
+	    try {
+	         Connection con = DBConnection.getCon();
+	         String sql="select * from bank_account where acc_no=?";
+	         PreparedStatement ps = con.prepareStatement(sql);
+	         ps.setInt(1, accountNumber);
+	         ResultSet rs=ps.executeQuery();
+	         while(rs.next()) {
+	             System.out.println("Account Number: "+rs.getInt(1));
+	             System.out.println("Customer Name: "+rs.getString(2));
+	             System.out.println("Account Type: "+rs.getString(3));
+	             System.out.println("Balance: "+rs.getFloat(4));
+	             
+	         }
+	         
+	     }catch (SQLException e) {
+	           // TODO Auto-generated catch block
+	           e.printStackTrace();
+	       }
+	         
+	}
+	public static void getAccountInfoByCustomerName(String name) {
+	    try {
+	         Connection con = DBConnection.getCon();
+	         String sql="select * from bank_account where acc_holder_name=?";
+	         PreparedStatement ps = con.prepareStatement(sql);
+	         ps.setString(1, name);
+	         ResultSet rs=ps.executeQuery();
+	         while(rs.next()) {
+	             
+	             System.out.println();
+	             System.out.println("Account Number: "+rs.getInt(1));
+	             System.out.println("Customer Name: "+rs.getString(2));
+	             System.out.println("Account Type:"+rs.getString(3));
+	             System.out.println("Balance:"+rs.getFloat(4));
+	             
+	         }
+	     }catch (SQLException e) {
+	          // TODO Auto-generated catch block
+	          e.printStackTrace();
+	      }
+	
+	}
+	
+	public static void getAllAccountsInfo() {
+	    try {
+	        Connection con = DBConnection.getCon();
+	         String sql="select * from bank_account ";
+	         PreparedStatement ps = con.prepareStatement(sql);
+	         ResultSet rs=ps.executeQuery();
+	         while(rs.next()) {
+	        	 System.out.println();
+	             System.out.println("Account Number: "+rs.getInt(1));
+	             System.out.println("Customer Name: "+rs.getString(2));
+	             System.out.println("Account Type:"+rs.getString(3));
+	             System.out.println("Balance:"+rs.getFloat(4));
+	         }
+	    }catch (SQLException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    }
+	}
+	
+	public static int withdrawal(float balance,int accountNumber) {
+	     int rowsUpdated=0;
+	     float funds=0;
+	     try {
+	         Connection con = DBConnection.getCon();
+	         String sql1="select balance from bank_account where acc_no=?";
+	         String sql="update  bank_account set balance=? where acc_no=?";
+	         PreparedStatement ps = con.prepareStatement(sql);
+	         PreparedStatement ps1 = con.prepareStatement(sql1);
+	         ps1.setInt(1,accountNumber);
+	         ResultSet rs=ps1.executeQuery();
+	         while(rs.next()) {
+	              funds=rs.getFloat(1);
+	             
+	         }
+	         funds=funds-balance;
+	         
+	         ps.setDouble(1,funds);
+	            ps.setInt(2, accountNumber);
+	            
+	            rowsUpdated=ps.executeUpdate();
+	     }catch(SQLException e) {
+	         e.printStackTrace();    
+	     }
+	     if(rowsUpdated>0) {
+	    	 System.out.println("Transaction is Successful");
+	     }
+	     return rowsUpdated;
+	}
+	
+    public static int deposit(float balance,int accountNumber) {
+        int rowsUpdated=0;
+        float funds=0;
+        try {
+            Connection con = DBConnection.getCon();
+            String sql1="select balance from bank_account where acc_no=?";
+            String sql="update  bank_account set balance=? where acc_no=?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            PreparedStatement ps1 = con.prepareStatement(sql1);
+            ps1.setInt(1,accountNumber);
+            ResultSet rs=ps1.executeQuery();
+            while(rs.next()) {
+                 funds=rs.getFloat(1);
+                
+            }
+             funds=funds+balance;
+            
+            ps.setDouble(1,funds);
+            ps.setInt(2, accountNumber);   
+            rowsUpdated=ps.executeUpdate();
+        }catch(SQLException e) {
+            e.printStackTrace();    
+        }
+        if(rowsUpdated > 0) {
+        	System.out.println("Deposited Successfully");
+        }
+        return rowsUpdated;
+    }
+
+
+	
 	
 }
--- banking/src/demo/Main.java
+++ banking/src/demo/Main.java
@@ -1,5 +1,6 @@
 package demo;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
@@ -8,22 +9,33 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		int choice = 0;
+		int accountNumber;
+		String accountHolderName;
+		String accountType;
+		float balance;
+		
 		do {
 			System.out.println("Welcome to Bank Application !!!!");
 			System.out.println("1. Create Account");
-			System.out.println("2. Exit");
+			System.out.println("2. Get Balance");
+			System.out.println("3. Withdraw");
+			System.out.println("4. Deposit");
+			System.out.println("5. Get Account Details By Account Number");
+			System.out.println("6. Get Account Details By Customer name");
+			System.out.println("7. Get All Account Details");
+			System.out.println("8. Exit");
 			System.out.println("Enter Your Choice: ");
 			choice = sc.nextInt();
 			switch(choice) {
 			case 1 : 
 				System.out.println("Enter Account Number: ");
-				int accountNumber = sc.nextInt();
+				accountNumber = sc.nextInt();
 				System.out.println("Enter Account Holder Name: ");
-				String accountHolderName = sc.next();
+				accountHolderName = sc.next();
 				System.out.println("Enter Account type (savings/current)");
-				String accountType = sc.next();
+				accountType = sc.next();
 				System.out.println("Enter Initial Balance: ");
-				float balance = sc.nextFloat();
+				balance = sc.nextFloat();
 				try {
 					BankAccount account = new BankAccount(accountNumber, accountHolderName, accountType, balance);
 					int rows = BankAccountOperations.addAccount(account);
@@ -38,8 +50,49 @@ public class Main {
 					e.printStackTrace();
 				}
 				break;
-			case 2 :
-			
+			case 2:
+				System.out.println("Enter Account Number: ");
+				accountNumber = sc.nextInt();
+				try {
+					BankAccountOperations op = new BankAccountOperations();
+					balance = BankAccountOperations.getBalance(accountNumber);
+					if(balance >= 0) {
+						System.out.println("Balance for "+accountNumber+" is:"+balance);
+					}else {
+						System.out.println(accountNumber+" does Not Exist");
+					}
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+				break;
+			case 3 :
+				System.out.println("Enter Account Number: ");
+				accountNumber = sc.nextInt();
+				System.out.println("Enter Amount to be Withdrawn: ");
+				float withdrawlAmount = sc.nextFloat();
+				BankAccountOperations.withdrawal(withdrawlAmount, accountNumber);
+				break;
+			case 4 :
+				System.out.println("Enetr Account Number: ");
+				accountNumber = sc.nextInt();
+				System.out.println("Enter Amount to be Deposited: ");
+				float depositAmount = sc.nextFloat();
+				BankAccountOperations.deposit(depositAmount, accountNumber);
+				break;
+			case 5 :
+				System.out.println("Enter Account Number: ");
+				accountNumber = sc.nextInt();
+				BankAccountOperations.getAccountInfoByAccountNumber(accountNumber);
+				break;
+			case 6 : 
+				System.out.println("Enter Customer Name: ");
+				accountHolderName = sc.next();
+				BankAccountOperations.getAccountInfoByCustomerName(accountHolderName);
+				break;
+			case 7 :
+				BankAccountOperations.getAllAccountsInfo();
+				break;
+			case 8 :
 				System.exit(1);
 			default : System.out.println("Invalid Choice");
 			}
